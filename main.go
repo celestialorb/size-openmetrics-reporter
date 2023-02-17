2ee405f5c10713efb2393bc9cf7959452046138a
celestialorb <chris.m.busby@gmail.com>
2023-02-17 15:57:55 -0800

    adding derived metric and more config

--- main.go
+++ main.go
@@ -13,11 +13,31 @@ import (
 	"github.com/spf13/viper"
 )
 
+var (
+	binAddrMetric = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Help: "The address of the section of the binary.",
+		Name: "elf_binary_section_addr",
+	}, []string{"label", "unit"})
+
+	binSizeMetric = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Help: "The size of the section of the binary.",
+		Name: "elf_binary_section_size",
+	}, []string{"label", "unit"})
+
+	ramUsageMetric = prometheus.NewGauge(prometheus.GaugeOpts{
+		Help: "The percentage usage of RAM by the binary from 0-1.",
+		Name: "elf_binary_ram_usage",
+	})
+)
+
 func main() {
 	pattern := regexp.MustCompile(`^\.(?P<label>\w+)\s+(?P<size>\d+)\s+(?P<addr>\d+)$`)
 
 	pflag.CommandLine.String("metrics.outfile", "out.prom", "The filename of the output OpenMetrics file.")
 	pflag.CommandLine.String("report.infile", "memory.stats", "The filename of the input memory report file.")
+	pflag.CommandLine.Bool("metrics.derived.ram", false, "Include a derived RAM usage metric.")
+	pflag.CommandLine.Bool("metrics.sections.addrs", false, "Include metrics on the section addresses of the binary.")
+	pflag.CommandLine.Bool("metrics.sections.sizes", true, "Include metrics on the section sizes of the binary.")
 	pflag.Parse()
 	err := viper.BindPFlags(pflag.CommandLine)
 	if err != nil {
@@ -28,16 +48,9 @@ func main() {
 
 	// Create our Prometheus registry and register our metrics.
 	registry := prometheus.NewRegistry()
-	binAddrMetric := prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Help: "The address of the section of the binary.",
-		Name: "elf_binary_section_addr",
-	}, []string{"label", "unit"})
-	binSizeMetric := prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Help: "The size of the section of the binary.",
-		Name: "elf_binary_section_size",
-	}, []string{"label", "unit"})
 	registry.MustRegister(binAddrMetric)
 	registry.MustRegister(binSizeMetric)
+	registry.MustRegister(ramUsageMetric)
 
 	report, err := os.Open(viper.GetString("report.infile"))
 	if err != nil {
@@ -46,6 +59,18 @@ func main() {
 
 	scanner := bufio.NewScanner(report)
 	scanner.Split(bufio.ScanLines)
+
+	// Keep our data in memory in a map.
+	addrs := map[string]float64{
+		"dma":      float64(0),
+		"heap":     float64(0),
+		"relocate": float64(0),
+	}
+	sizes := map[string]float64{
+		"dma":      float64(0),
+		"heap":     float64(0),
+		"relocate": float64(0),
+	}
 
 	// Scan each line.
 	for scanner.Scan() {
@@ -70,21 +95,52 @@ func main() {
 				logrus.Fatal(err)
 			}
 
-			// Update our metrics.
+			// Update our in-memory map.
+			label := parts[1]
+			addrs[label] = addr
+			sizes[label] = size
+		}
+	}
+	report.Close()
+
+	// Update our binary section address metrics if we're configured to do so.
+	if viper.GetBool("metrics.sections.addrs") {
+		for label, addr := range addrs {
 			binAddrMetric.With(prometheus.Labels{
-				"label": parts[1],
+				"label": label,
 				"unit":  "byte",
 			}).Set(addr)
+		}
+	}
 
+	// Update our binary section size metrics if we're configured to do so.
+	if viper.GetBool("metrics.sections.sizes") {
+		for label, size := range sizes {
 			binSizeMetric.With(prometheus.Labels{
-				"label": parts[1],
+				"label": label,
 				"unit":  "byte",
 			}).Set(size)
 		}
 	}
 
-	report.Close()
+	// Update our derived metrics if we're instructed to do so.
+	if viper.GetBool("metrics.derived.ram") {
+		logrus.Info("calculating derived RAM metric")
 
+		usage := (addrs["heap"] - addrs["relocate"] + sizes["heap"])
+		total := addrs["dma"] - addrs["relocate"]
+
+		logrus.WithFields(logrus.Fields{
+			"usage": usage,
+			"total": total,
+		}).Debug("calculated metric")
+
+		// Calculate the RAM usage of the binary.
+		logrus.Info("setting derived RAM metric")
+		ramUsageMetric.Set(usage / total)
+	}
+
+	// Write out the Prometheus metrics to a file.
 	err = prometheus.WriteToTextfile(viper.GetString("metrics.outfile"), registry)
 	if err != nil {
 		logrus.Error(err)
